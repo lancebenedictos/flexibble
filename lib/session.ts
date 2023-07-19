2827e1b4ca5a09e8e705e2889ee29b535d6723a7
lancebenedictos <lancebenedictos@gmail.com>
2023-07-19 19:52:28 -0400

    auth logo fixed

--- lib/session.ts
+++ lib/session.ts
@@ -33,7 +33,7 @@ export const authOptions: NextAuthOptions = {
   },
   theme: {
     colorScheme: "light",
-    logo: "./logo.png",
+    logo: "./logo.svg",
   },
   callbacks: {
     async session({ session }) {
